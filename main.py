c50d745d66886f93d2d9b3c8b3e29af3d74f5bdd
asiergpt <130127305+asiergpt@users.noreply.github.com>
2026-03-16 06:30:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Networking · Asier Dorronsoro",
+    page_title="Networking | Asier Dorronsoro",
     page_icon="🗺️",
     layout="wide",
     initial_sidebar_state="collapsed"
